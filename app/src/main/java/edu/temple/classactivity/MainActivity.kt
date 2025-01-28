4b90010dc01f318bc21929dc49d6b9ebb8c5d259
KYWILS21 <tuk30460@temple.edu>
2025-01-28 12:18:48 -0500

    Added functionality so that the button is able to change its own text to Button Clicked

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,28 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById<Button>(R.id.button)).setOnClickListener(){
+            (it as Button).text = "Button Clicked"
+        }
+
+
     }
+
+
 }
